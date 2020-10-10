f34d1f9777b65a830b5ef08f8a1e30983bca6393
Christoph Neumüller <c.neumueller@gmail.com>
2020-10-10 14:45:19 +0200

    bugfixes

--- src/BrowserGameEngine.StatefulGameServer/Repositories/Units/UnitRepository.cs
+++ src/BrowserGameEngine.StatefulGameServer/Repositories/Units/UnitRepository.cs
@@ -26,10 +26,12 @@ namespace BrowserGameEngine.StatefulGameServer {
 		private IList<Unit> Units(PlayerId playerId) => world.GetPlayer(playerId).State.Units;
 
 		public IEnumerable<UnitImmutable> GetAll(PlayerId playerId) {
+			world.ValidatePlayer(playerId);
 			return Units(playerId).Select(x => x.ToImmutable());
 		}
 
 		public bool PrerequisitesMet(PlayerId playerId, UnitDef unitDef) {
+			world.ValidatePlayer(playerId);
 			if (unitDef.PlayerTypeRestriction != playerRepository.GetPlayerType(playerId)) return false;
 			foreach (var prereq in unitDef.Prerequisites) {
 				if (!assetRepository.HasAsset(playerId, prereq)) return false;
@@ -38,34 +40,42 @@ namespace BrowserGameEngine.StatefulGameServer {
 		}
 
 		public IEnumerable<UnitImmutable> GetByUnitDefId(PlayerId playerId, UnitDefId unitDefId) {
+			world.ValidatePlayer(playerId);
 			return Units(playerId)
 				.Where(x => x.UnitDefId.Equals(unitDefId))
 				.Select(x => x.ToImmutable());
 		}
 
 		public IEnumerable<UnitImmutable> GetById(PlayerId playerId, UnitId unitId) {
+			world.ValidatePlayer(playerId);
 			return Units(playerId)
 				.Where(x => x.UnitId == unitId)
 				.Select(x => x.ToImmutable());
 		}
 
 		public int CountByUnitDefId(PlayerId playerId, UnitDefId unitDefId) {
+			world.ValidatePlayer(playerId);
 			return Units(playerId)
 				.Where(x => x.UnitDefId.Equals(unitDefId))
 				.Sum(x => x.Count);
 		}
 
 		public IEnumerable<UnitDef> GetUnitsPrerequisitesMet(PlayerId playerId) {
+			world.ValidatePlayer(playerId);
 			return gameDef.GetUnitsByPlayerType(world.GetPlayer(playerId).PlayerType).Where(x => PrerequisitesMet(playerId, x));
 		}
 
 		public IEnumerable<UnitImmutable> GetAttackingUnits(PlayerId playerId, PlayerId enemyPlayerId) {
+			world.ValidatePlayer(playerId);
+			world.ValidatePlayer(enemyPlayerId);
 			return Units(playerId)
 				.Where(x => x.Position == enemyPlayerId)
 				.Select(x => x.ToImmutable());
 		}
 
 		public IEnumerable<UnitImmutable> GetDefendingEnemyUnits(PlayerId playerId, PlayerId enemyPlayerId) {
+			world.ValidatePlayer(playerId);
+			world.ValidatePlayer(enemyPlayerId);
 			if (!GetAttackingUnits(playerId, enemyPlayerId).Any()) throw new CannotViewEnemyBaseException();
 			return Units(enemyPlayerId)
 				.Where(x => x.IsHome())
